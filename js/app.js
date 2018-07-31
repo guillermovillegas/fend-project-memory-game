0839aea764dbc9c3e4b36cb9fed5a6107c888297
Descartes <guillermovillegas773@gmail.com>
2018-07-30 19:10:24 -0500

    programmatically created cards

--- js/app.js
+++ js/app.js
@@ -1,14 +1,45 @@
-/*
- * Create a list that holds all of your cards
- */
-const array = 
+// programatically create a card FEND P3 Mike Wales tutorial
+const card = ['fa-diamond', 'fa-diamond',
+						 'fa-paper-plane-o', 'fa-paper-plane-o',
+						 'fa-anchor', 'fa-anchor',
+						 'fa-bolt', 'fa-bolt',
+						 'fa-cube', 'fa-cube',
+						 'fa-leaf', 'fa-leaf',
+						 'fa-bicycle','fa-bicycle',
+						 'fa-bomb', 'fa-bomb',
+						 ];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function generateCards(card) {
+	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`
+
+};
+
+// append cards to the deck node FEND P3 Mike Wales tutorial
+function initGame() {
+	const deck = document.querySelector('.deck');
+	let cardHTML = card.map(function(card) {
+		return generateCards(card);
+	});
+	deck.innerHTML = (cardHTML.join(''));
+}
+
+initGame();
+
+function createBoard(cards) {
+	const cardParent = document.querySelector('.deck');
+	cardParent.appendChild(card);
+};
+
+
+// add event listeners to the cards
+const allCards = document.querySelectorAll('.card');
+const openCards = [];
+
+allCards.forEach(function(card) {
+	card.addEventListener('click', function (e) {
+		card.classList.add('open', 'show');
+	})
+});
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -23,33 +54,7 @@ function shuffle(array) {
     }
 
     return array;
-}
+};
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-const allCards = document.querySelectorAll('.card');
 
-allCards.forEach(function(card) {
-	card.addEventListener('click', function (e) {
-		card.classList.add('open', 'show');
-	})
-});
-
-/*const restart = document.querySelectorAll('.restart');
-
-restart.forEach(function(restartGame) {
-	restartGame.addEventListener('click', function(e) {
-		const allCards = document.querySelectorAll('.card');
-		allCards.classList.remove('open', 'show');
-		console.log('restart');
-	})
-});*/
